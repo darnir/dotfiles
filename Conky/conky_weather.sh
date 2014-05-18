8921a58c0bda165708f161ae21c2717227ffd4a0
Darshit Shah <darnir@gmail.com>
2014-05-18 23:24:53 +0530

    [conky_weather] Change default city back to Mumbai

--- Conky/conky_weather.sh
+++ Conky/conky_weather.sh
@@ -3,7 +3,7 @@
 MUMID=2295411
 HYDID=2295414
 KLID=664474
-WOEID=${KLID}
+WOEID=${MUMID}
 
 curl -s "http://weather.yahooapis.com/forecastrss?w=${WOEID}&u=c" -o ~/.cache/weather.xml.tmp
 grep -q '<rss version="2.0" xmlns:yweather="http://xml.weather.yahoo.com/ns/rss/1.0" xmlns:geo="http://www.w3.org/2003/01/geo/wgs84_pos#">' ~/.cache/weather.xml.tmp
