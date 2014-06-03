77ad52d39bf436a2bf11201eb187037795c6989b
Darshit Shah <darnir@gmail.com>
2014-06-03 15:32:59 +0530

    [InstallScripts] Fix template install script

--- InstallScripts/template.sh
+++ InstallScripts/template.sh
@@ -40,40 +40,21 @@ sudo ln -s ${_LOC_FILE} ${_CONF_FILE}
 
 ################################################################################
 
-############################## File Block ######################################
+############################## Directory Block #################################
 
-_CONF_NAME=makepkg.conf
-_CONF_FILE=${SYSCONFDIR}/${_CONF_NAME}
-_BCK_FILE=${_CONF_FILE}.bck
-_LOC_FILE=$(pwd)/${PACKAGE_NAME}/${_CONF_NAME}
+_CONF_NAME=vim
+_CONF_DIR=${UHOME}/.${_CONF_NAME}
+_BCK_DIR=${_CONF_DIR}bck
+_LOC_DIR=$(pwd)/${PACKAGE_NAME}/${_CONF_NAME}
 
-echo "Checking if ${_CONF_FILE} exists"
-if [ -f ${_CONF_FILE} ]
+echo "Checking if ${_CONF_DIR} exists"
+if [ -d ${_CONF_DIR} ]
 then
-    echo "File found, saving to ${_BCK_FILE}"
-    sudo mv ${_CONF_FILE} ${_BCK_FILE}
+    echo "Directory found, saving to ${_BCK_DIR}"
+    sudo mv ${_CONF_DIR} ${_BCK_DIR}
 fi
 
-echo "Linking dotfile ${_CONF_NAME} to ${_CONF_FILE}"
-sudo ln -s ${_LOC_FILE} ${_CONF_FILE}
-
-################################################################################
-
-############################## File Block ######################################
-
-_CONF_NAME=mirrorlist
-_CONF_FILE=${SYSCONFDIR}/pacman.d/${_CONF_NAME}
-_BCK_FILE=${_CONF_FILE}.bck
-_LOC_FILE=$(pwd)/${PACKAGE_NAME}/pacman.d/${_CONF_NAME}
-
-echo "Checking if ${_CONF_FILE} exists"
-if [ -f ${_CONF_FILE} ]
-then
-    echo "File found, saving to ${_BCK_FILE}"
-    sudo mv ${_CONF_FILE} ${_BCK_FILE}
-fi
-
-echo "Linking dotfile ${_CONF_NAME} to ${_CONF_FILE}"
-sudo ln -s ${_LOC_FILE} ${_CONF_FILE}
+echo "Linking dotfile ${_CONF_NAME} to ${_CONF_DIR}"
+sudo ln -s ${_LOC_DIR} ${_CONF_DIR}
 
 ################################################################################
