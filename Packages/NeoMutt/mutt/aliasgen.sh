32be6ffc7b5b6514328d8dc9deae732ccf03235d
Darshit Shah <darnir@gmail.com>
2016-09-06 15:22:53 +0200

    [NeoMutt] Update alias generator filters

--- Packages/NeoMutt/mutt/aliasgen.sh
+++ Packages/NeoMutt/mutt/aliasgen.sh
@@ -15,8 +15,11 @@ MAIL=$(awk '{print $(NF)}' <<< "$ALIASLINE" | tr -d '<>')
 
 # We never want to create some aliases
 NOALIAS_PAT="facebook|twitter|emirates|amazon|gnu|github|sofort|paypal| \
-	notebooksbilliger|online.net|no[,-]reply|meetup|jetprivilege| \
-	stackexchange|shopping"
+	notebooksbilliger|online.net|noreply|no-reply|meetup|jetprivilege| \
+	stackexchange|shopping|airbnb|aur.archlinux.org|printer@cs.fs| \
+    quora|coverity|bits2msphd|do-not-reply|flixbus|piazza|debbugs|contact@| \
+	gitter|googlegroups|keybase|linkedin|mailman|neomutt|travis| \
+	guerrillamail|webmaster|buildbot|arch-general|arch-dev-public|payoneer"
 
 if grep -Eq "$NOALIAS_PAT" <<< "$ALIASLINE"; then
 	:
